f8c96c4035b3025e52563fb7ab053773410543e1
Ankur Charan <ankurcharan98@gmail.com>
2019-11-24 06:35:51 +0530

    ui and fixes
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- getYoutubeVideos.js
+++ getYoutubeVideos.js
@@ -39,81 +39,50 @@ function getVideo(trackArray){
 
     return new Promise((resolve22, reject22) => {
 
+        let result = []
+        let promises = []
+        for(let i in trackArray) {
 
-        // let result = []
-        // let promises = []
-        // for(let i in trackArray) {
+            // TODO Remove
+            if(i == 2) {
+                break;
+            }
 
-        // TODO Remove
-        //     if(i == 2) {
-        //         break;
-        //     }
+            let pro = getOneVideoId(trackArray[i]).then((res) => {
+                result.push(res)
+            }).catch((err) => {
+                console.log('Getting VideoID Error: ', err);
+            });
 
-        //     let pro = getOneVideoId(trackArray[i]).then((res) => {
-        //         result.push(res)
-        //     }).catch((err) => {
-        //         console.log('Getting VideoID Error: ', err);
-        //     });
+            promises.push(pro)
+        }
 
-        //     promises.push(pro)
-        // }
+        Promise.all(promises)
+        .then(() => {
 
-        // Promise.all(promises)
-        // .then(() => {
-            // console.log('idddddddddddds', result)
-            // return result
+            // let fakeData = [ 
+            //     { 
+            //         id: 'uDLgA_YFuuA',
+            //         title: 'Mark Medlock - Maria Maria (Videoclip)' 
+            //     }, { 
+            //         id: 'QOowQeKyNkQ',
+            //         title: 'Chris Brown - Don&#39;t Wake Me Up (Official Music Video)' 
+            //     }, {
+            //         id: 'BSOhuXgkWcE',
+            //         title: 'bomb bae'
+            //     }
+            // ];
 
-            let fakeData = [ 
-                { 
-                    id: 'uDLgA_YFuuA',
-                    title: 'Mark Medlock - Maria Maria (Videoclip)' 
-                }, { 
-                    id: 'QOowQeKyNkQ',
-                    title: 'Chris Brown - Don&#39;t Wake Me Up (Official Music Video)' 
-                }, {
-                    id: 'BSOhuXgkWcE',
-                    title: 'bomb bae'
-                }
-            ];
+            // resolve22(fakeData);
 
-            resolve22(fakeData);
+            resolve22(result)
+        }).catch((err) => {
 
-            // resolve22(result)
-        // }).catch((err) => {
-
-        //     console.log('Promise All Resolve Error: ', err);
-        //     // return []
-        //     reject22(err);
-        // });
-    
-    
+            console.log('Promise All Resolve Error: ', err);
+            // return []
+            reject22(err);
+        });
     });
-
-    // getOneVideoId(trackArray[0])
-    // .then((s) => console.log(s))
-    // .catch(err => console.log(err));
-        //     let urls = []
-    //     //for(let track in trackArray){
-    
-    //         search(trackArray[0], opts, function(err, results) {
-    //             if(err){
-    //                 console.log(err)
-    //             }
-    //             else{
-    //                 //results = JSON.parse(results)
-    //                 console.log(results)
-    //                 let id = results[0]['id']
-    //                 let title = results[0]['title']
-    //                 urls.push({
-    //                     id : id,
-    //                     title : title
-    //                 })
-    //             }
-    //         });
-    //     // }        
-    // })
-
-
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -3,30 +3,45 @@ var app = express()
 var rp = require('request-promise')
 let request = require('request');
 var getYoutubeVideos = require('./getYoutubeVideos')
+let config = require('./config');
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 app.use(express.static('./public'))
 
-var emotion = null
-
 function getEmotion() {
 
-    return new Promise((res, rej) => {
+    return new Promise((resolve, reject) => {
 
-        console.log("Camera opened")
-        var spawn = require("child_process").spawn; 
-        var process = spawn('python',["./face_recog.py"] );
+        console.log("Camera opening")
+        
+        // Enter your mood here
+        if(config.camera === false) {
+            resolve('Neutral');
+            return;
+        }
 
+        let spawn = require("child_process").spawn; 
+
+        let process = null;
+        if(config.python3)
+            process = spawn('python3', ["./face_recog.py"] );
+        else
+            process = spawn('python', ['./face_recog.py']);
+
+        let emotion = null
         process.stdout.on('data', function(data) { 
-            console.log(data.toString())
-            emotion = data.toString() 
 
-            if(emotion == undefined)
-            rej(emotion)
+            emotion = data.toString().trim();
+
+            if(config.debug)
+                console.log(emotion, ' emotion detected');
+
+            if(emotion === undefined || emotion === null)
+                reject(emotion)
             else
-            res(emotion)
+                resolve(emotion)
         })
     })
 }
@@ -39,7 +54,7 @@ function getSongsList(valence,arousal) {
         request(api_url, (err, respo, body) => {
 
             if(err) {
-                console.log('could make musiccovery rqst');
+                console.log('Could not make Musicovery API Request');
                 reject(err)
             }
 
@@ -48,75 +63,112 @@ function getSongsList(valence,arousal) {
     });
 }
 
-function openCamera(req,res){
+function openCamera(request, response){
 
     getEmotion()
     .then((emotion) => {
-        console.log(emotion)
-        emotion = 'Happy'
+        console.log(typeof(emotion))
+        console.log(emotion.length)
+        // emotion = 'Happy'
 
-        valence = 0
-        arousal = 0
+        let valence = null
+        let arousal = null
 
-        if(emotion == 'Happy')
-        {
+        if(emotion === 'Happy') {
             valence = 900000
             arousal = 900000
-        }
-        else if(emotion == 'Sad')
-        {
+        } else if(emotion === 'Sad') {
             valence = 200000
             arousal = 200000
-        }
-        else if(emotion == 'Angry')
-        {
+        } else if(emotion === 'Angry') {
             valence = 100000
             arousal = 700000
-        }
-        else if(emotion == 'Neutral')
-        {
+        } else if(emotion === 'Neutral') {
             valence = 700000
             arousal = 200000
         }
+
+        if(config.debug)
+            console.log('Valence: ', valence, ', Arousal: ', arousal);
 
         getSongsList(valence, arousal)
         .then((data) => {
 
             data = JSON.parse(data);
-            // console.log('data', data);
-            var trackList = []
-            var tracks = data['tracks']['track']
-            for(var track in tracks){
-                
-                if(track['title'] == '' || track['artist_display_name'] == '')
+
+            if(config.debug)
+                console.log('Musicovery API data: ', data);
+
+            let trackList = []
+            let tracks = data['tracks']['track'];
+
+            if(config.debug)
+                console.log('Musicovery API tracks: ', tracks);
+            
+            for(let track in tracks) {
+                           
+                if(tracks[track]['title'] == '' || tracks[track]['artist_display_name'] == '')
 				    continue
                 var song = tracks[track]['artist_display_name'] + ' ' + tracks[track]['title'];
-                // trackList += song
                 trackList.push(song);
             }
 
-            // console.log(trackList);
+            if(config.debug)
+                console.log('trackList', trackList);
 
             getYoutubeVideos.getVideo(trackList)
             .then((videoData) => {
-                console.log(videoData)
-                res.json({
+
+                if(config.debug)
+                    console.log(videoData);
+
+                return response.status(200).json({
                     success : true,
                     data : videoData
                 })
             })
             .catch((err) => {
-                console.log('Error getting video data from youtube: ', err);
+                return console.log('Error getting video data from youtube: ', err);
             });
         })
         .catch((err) => {
-            console.log(err)
+            return console.log(err)
         });
     })
     .catch((err) => {
-        console.log(err)
+        return console.log(err)
     });
 }
+
+function fuck(req, res) {
+
+
+    setTimeout(() => {
+
+        return res.json({
+            success: true,
+            data: [
+                {
+                  id: 'uDLgA_YFuuA',
+                  title: 'Mark Medlock - Maria Maria (Videoclip)'
+                },
+                {
+                  id: 'QOowQeKyNkQ',
+                  title: 'Chris Brown - Don&#39;t Wake Me Up (Official Music Video)'
+                },
+                { 
+                    id: 'BSOhuXgkWcE', 
+                    title: 'bomb bae' 
+                }
+            ]
+              
+        });
+
+    }, 3000);
+}
+
+
+// app.use('/api', fuck);
 
 app.use('/api', openCamera)
 
