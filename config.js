073ddfaf5d7516efb110ec4905179899742506c9
gaurav <gogarattan@gmail.com>
2020-05-22 22:56:46 +0530

    Final Code

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	debug: true,		// if yout want debug outputs on terminal
-	python3: true,		// if you want to use python2, make false
+	python3: false,		// if you want to use python2, make false
 	camera: true,		// if want to open camera
 	cameraOffMood: 'Neutral',	// if camera is off, this mood will be used for further processing
 	saveYoutubeRequest: true,	// if true, no youtube requests will be made, fake Data is returned
